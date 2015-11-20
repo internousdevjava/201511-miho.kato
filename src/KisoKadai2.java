43de37ddfa40d2f035a1d853759bebf6463ec4b5
Miho Kato <ipc.loyalty@gmail.com>
2015-11-20 12:55:20 +0900

    基礎課題1

--- src/KisoKadai2.java
+++ src/KisoKadai2.java
@@ -25,10 +25,14 @@ public class KisoKadai2 {
 		int c=0;
 
 
+
 		r= new java.util.Random().nextInt(100);
 		do{
+			System.out.println("1～100の数値を入力して下さい！");
 
-			try{
+			do{
+
+				try{
 					BufferedReader	br=new	BufferedReader(new InputStreamReader(System.in)); //(System.in)コンソールから文字列を受け付ける
 
 					if(c==0){
@@ -36,30 +40,44 @@ public class KisoKadai2 {
 					}
 
 					else{
-						System.out.println(r+"もう一度入力して下さい！\n");
+						if(a<0 || a>100){
+							System.out.println("正しい数値を入れてください！");
+						}
+						System.out.println("もう一度入力して下さい！\n");
+
 					}
 
-					System.out.println("1～100の数値を入力して下さい！");
+					//System.out.println("1～100の数値を入力して下さい！");
 					System.out.print("入力値:");
 					kazu=br.readLine();
 
 
-			}
-			catch(Exception e){
-				e.printStackTrace();
-			}
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
 
-			a = Integer.parseInt(kazu);
+				a = Integer.parseInt(kazu);
 
-			if(a<r){
-				System.out.println("正解の数より小さいです");
-			}
-			else if(a>r){
-				System.out.println("正解の数より大きいです");
-			}
-			else{
-				System.out.println("正解です！");
-			}
+
+				if(a<0 || a<100){
+
+					if(a<r){
+						System.out.println("正解の数より小さいです");
+					}
+
+					else if(a>r){
+						System.out.println("正解の数より大きいです");
+					}
+
+					else{
+						System.out.println("正解です！");
+					}
+				}
+
+			}while(!(a==r));
+
+
 
 		}while(!(a==r));
 
